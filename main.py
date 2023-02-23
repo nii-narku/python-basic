bfe839b3c6d6d81c4306a270c87981b48424c5af
nii-narku <niinarku969@gmail.com>
2023-02-23 17:15:36 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+first_name = "Nii Narku"
+last_name = "Noi"
+phone_number = 269134014
+age = 21
+
+#How to create a list of multiple elements.
+names_of_students = ["nii narku noi","gyamfi michael","asare kofi obeng"]
+
+#List index starts from 0. So (0,1,2,3...).
+#Selecting the second student on the list and assigning a variable "second_student" to it.
+second_student = names_of_students[1]
+print(second_student)
+
+#Adding a new student to the list of students at the end. Use .append
+names_of_students.append("newman")
+print(names_of_students)
