7545e0e2c9007d1dbd76ec3aff20998913fef669
nii-narku <niinarku969@gmail.com>
2023-03-09 17:08:56 +0000

    Added list and dictionary

---
+++ main2.py
@@ -0,0 +1,12 @@
+#Creating a list of 5 students, print, adding anmother student, and select a third student
+#saving it in a variable called [third_student], remove the last student and print
+
+names_of_students = ["Narku","Asare","Michael","Phinehas","Desmond"]
+print(names_of_students)
+names_of_students.push("Kenneth")
+print(names_of_students)
+third_student = names_of_students[2]
+print(third_student)
+
+#removing last element. Use ".pop"
+names_of_students.pop[4]
---
+++ main3.py
@@ -0,0 +1,15 @@
+#Grading students
+students_grade = {}
+
+#adding to an empty dictionary
+students_grade["Mike"] = "A"
+students_grade["Aaron"] = "B"
+students_grade["Nii"] = "C"
+print(students_grade)
+
+#removing an element from the dictionary
+students_grade.pop("Aaron")
+
+#Taking inputs from a user
+age = input("what is your age?\n")    
+print(f"Your age is {age}")
