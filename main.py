86e262fe99fb7d25a18c973f3ba2977f38db696a
nii-narku <niinarku969@gmail.com>
2023-03-09 17:00:46 +0000

    Added list and dictionary

--- main.py
+++ main.py
@@ -14,3 +14,42 @@ print(second_student)
 #Adding a new student to the list of students at the end. Use .append
 names_of_students.append("newman")
 print(names_of_students)
+
+#Creating a list of 5 students, print, adding anmother student, and select a third student
+#saving it in a variable called [third_student], remove the last student and print
+
+names_of_students = ["Narku","Asare","Michael","Phinehas","Desmond"]
+print(names_of_students)
+names_of_students.push("Kenneth")
+print(names_of_students)
+third_student = names_of_students[2]
+print(third_student)
+
+#removing last element. Use ".pop"
+names_of_students.pop[4]
+
+#Python dictonary
+my_details = {"name":"Nii","age":15, "gender":"Male"}
+
+joels_info = {"name":"joel",
+                "age":13,
+                "hobbies": {"outdoor":["Roaming","ampe"],"indoor":["ludu","table_tennis"]}
+                }
+hobbies=joels_info["hobbies"]
+outdoor= hobbies["outdoor"]
+second_outdoor_hobby = outdoor[1]
+print(second_outdoor_hobby)
+
+#Grading students
+students_grade = {}
+
+#addong to the dictionary
+students_grade["Mike"] = "A"
+print(students_grade)
+students_grade["Aaron"] = "B"
+print(students_grade)
+students_grade["Nii"] = "C"
+print(students_grade)
+
+#reomoving from the dictionary
+students_grade.pop("Aaron")
